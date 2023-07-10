8b18b3573a19ec18b80a2f2f762ba30deb7d68a2
saber pourmomen <saber.pourmomen@gmail.com>
2023-07-10 22:47:21 +0800

    First initialization

---
+++ ArrayRotation/src/main/java/org/interview/Main.java
@@ -0,0 +1,7 @@
+package org.interview;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("hello world!");
+    }
+}
---
+++ ArrayRotation/src/test/java/org/interview/MainTest.java
@@ -0,0 +1,11 @@
+package org.interview;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+    @Test
+    void mainTest() throws Exception {
+        Main.main(null);
+    }
+}
